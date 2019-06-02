38dd939d18758640fc194fb0b3c85a5e8ddeeb99
happydesign2webstudio <51080346+happydesign2webstudio@users.noreply.github.com>
2019-06-02 20:48:20 +0200

    Create dbconfig.php

---
+++ datagrid/dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+//Modify constants with data needed to access your own database
+define('DB_HOST','localhost');
+define('DB_NAME','compositori');
+define('DB_USER','root');
+define('DB_PASSWORD','kumakassiokuno');
+?>
